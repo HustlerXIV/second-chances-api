f4ecf14b57c941c0b143e79b4316dd6233457ca6
TangNapat <napat.jaiyim@hotmail.com>
2024-09-20 22:48:07 +0700

    Created Put and Delete Methods for Pet

--- routes/pets.js
+++ routes/pets.js
@@ -3,10 +3,14 @@ const router = express.Router();
 const petsController = require('../controllers/petControllers');
 const authMiddleware = require('../middleware/authMiddleware');
 
-router.post('/', petsController.createPet);
+router.post('/', authMiddleware, petsController.createPet);
 
 router.get('/', authMiddleware, petsController.getAllPets);
 
 router.get('/:id', petsController.getPetById);
+
+router.put('/:id', authMiddleware, petsController.editPetById);
+
+router.delete('/:id', authMiddleware, petsController.deletePetById);
 
 module.exports = router;
