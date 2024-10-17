e31391cec5bf916ea5faaaa852e72d7f39d6831e
Napat Jaiyim <napat.jaiyim@coforge.com>
2024-10-17 09:23:26 +0700

    [UPDATE] Added default port

--- server.js
+++ server.js
@@ -4,18 +4,11 @@ const routes = require("./routes");
 const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 
 app.use(cors());
-
-// app.use(
-//   cors({
-//     origin: "http://localhost:3000",
-//     credentials: true,
-//   })
-// );
 
 app.use("/api", routes);
 
